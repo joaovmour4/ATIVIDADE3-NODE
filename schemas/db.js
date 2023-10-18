781d6212f1d827e30f4a873703938c882783f1c9
joaovmour4 <joaov.mour@unifesspa.edu.br>
2023-10-18 09:33:53 -0300

    Adição de CORS e utilização do dotenv

--- schemas/db.js
+++ schemas/db.js
@@ -1,4 +1,11 @@
+require('dotenv').config()
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postgres://postgres:password@localhost:5432/atv3-db', {dialect: 'postgres'})
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost:5432/atv3-db`, {
+    dialect: 'postgres'
+})
 
 module.exports = sequelize
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = require('./routes/routes')
 const db = require('./schemas/db')
+const cors = require('cors')
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(express.urlencoded({extended:true}))
